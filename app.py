a2d1693ecf2df9aebc54249e1062652ddcf76419
hophopenak <hoph5031@gmail.com>
2026-02-28 15:55:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,14 +57,6 @@ def inject_css():
         div[data-testid="stToolbar"] { display: none; }
 
         /* =========================
-           PENTING:
-           Jangan sembunyikan tombol panah collapse sidebar
-           (jadi baris ini DIHAPUS dari versi sebelumnya)
-           - button[data-testid="stSidebarCollapseButton"] ...
-           - div[data-testid="collapsedControl"] ...
-        ========================= */
-
-        /* =========================
            PINK SOFT + HIGH READABILITY
         ========================= */
         :root{
@@ -607,3 +599,4 @@ st.markdown(
 st.dataframe(prov_table, use_container_width=True, hide_index=True)
 
 st.caption("Catatan: Nilai provinsi dihitung dari rata-rata sederhana prediksi seluruh kabupaten/kota pada setiap tahun.")
+
